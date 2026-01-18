a4c80c4157c22704bdb6d6bc8e53e5d1f0cfa445
daniel-amekpoagbe-yawson <amekpoagbedaniel@gmail.com>
2026-01-18 15:44:34 +0000

    added mobile-app-0x04

---
+++ prodev-mobile-app-0x04/styles/index.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 16,
+  },
+  navGroup: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 20,
+    gap: 10,
+  },
+  largeText: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginVertical: 8,
+  },
+  smallText: {
+    fontSize: 14,
+    color: "#666",
+    marginVertical: 8,
+  },
+  formGroup: {
+    marginTop: 20,
+  },
+});
